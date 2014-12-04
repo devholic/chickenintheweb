0b8f13963d2e3693ee88a49a207e4eb910533a17
devholic : MBPr (Mac) <devholic@plusquare.com>
2014-12-04 17:43:17 +0900

    commit #9
    - template base 추가 (다른곳에서 import하는 방식으로)
    - 로그아웃 시 이전화면 그대로 남아있는 현상 수정 (response header)
    - 회원가입 추가
    - 비밀번호 찾기 기능 추가
    - hash 코드를 메소드로 변경 (기존에 메소드가 아닌 직접 hash 했었음)

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import os
 import webapp2
 import logging
 import random
 import datetime
+import time
 import hashlib
+import urllib2
 from webapp2_extras import sessions
+from google.appengine.api import mail
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
@@ -36,7 +24,12 @@ class User(db.Model):
 	isSeller = db.BooleanProperty() # 판매자인지 
 	isAdmin = db.BooleanProperty() # 관리자인지
 
-def Render(handler, path = "index.htm", values ={}):
+class UserRecovery(db.Model):
+	email = db.StringProperty() # 이메일
+	code = db.StringProperty() # 코드
+	expire = db.IntegerProperty()
+
+def Render(handler, path = "n_index.htm", values ={}):
 	fpath = os.path.join(os.path.dirname(__file__), 'templates/' + path)
 	if not os.path.isfile(fpath):
 		return False
@@ -44,11 +37,20 @@ def Render(handler, path = "index.htm", values ={}):
 	email = handler.session.get('email')
 	if email:
 		d['email'] = email
-
 	d['path'] = handler.request.path
 	outstr = template.render(fpath, d)
+	handler.response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	handler.response.headers["Pragma"] = "no-cache"
+	handler.response.headers["Expires"] = "0"
 	handler.response.out.write(unicode(outstr))
 	return True
+
+def Salt():
+	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	saltstring = ""
+	for i in range(16):
+		saltstring+=random.choice(alphabet)
+	return saltstring
 
 class BaseHandler(webapp2.RequestHandler):
     def dispatch(self):
@@ -66,36 +68,52 @@ class BaseHandler(webapp2.RequestHandler):
         # Returns a session using the default cookie key.
         return self.session_store.get_session()
 
-class RegisterHandler(webapp2.RequestHandler):
+class RegisterHandler(BaseHandler):
 	def get(self):
-		path = os.path.join(os.path.dirname(__file__), 'error.htm')
-		self.response.write(template.render(path,{'errorcode':'400'}))
+		Render(self, 'n_register.htm', {})
 
 	def post(self):
 		req_email = self.request.get('email')
-		alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-		saltstring = ""
-		for i in range(16):
-			saltstring+=random.choice(alphabet)
-
-		query = User.all()
-		if query.filter("email ==",req_email).count() == 1:
+		query = db.Query(User)
+		q = query.filter("email ==",email)
+		if q.count() == 1:
 			# 유저가 존재하는 경우
-			# 유저를 삭제 
+			u = q.get()
 			req_pw = self.request.get('password')
+			# 로그인 시도
+			if u.password == hashlib.sha256(req_pw + u.salt).hexdigest():
+				self.session['email'] = email
+				Render(self, 'n_index.htm', {})
+			else:
+			# 만약 아니라면 이미 있는 계정이라고 form update
+				$.ajax({
+				type: "POST",
+				url: "https://myapp.appspot.com/service",
+				contentType: "application/json; charset=utf-8",
+				data: data,
+				success: function(data) {
+				alert("AJAX done");
+				}
+				}); 
 		else:
 			# 유저를 추가
 			user = User()
 			user.email = self.request.get('email')
-			user.password = hashlib.sha256(self.request.get('password') + saltstring).hexdigest() # Hash 후 저장
+			user.password = hashlib.sha256(self.request.get('password') + Salt()).hexdigest() # Hash 후 저장
+			user.name = self.request.get('name')
+			user.address = self.request.get('address')
+			user.number = self.request.get('number')
 			user.salt = saltstring # Salt
 			user.isSeller = False # 판매자 False
 			user.isAdmin = False # 관리자 False
 			user.put()
+			self.session['email'] = email
+			self.session['name'] = name
+			Render(self, 'n_index.htm', {})
 
 class LoginHandler(BaseHandler):
 	def get(self):
-		Render(self, "index.htm")
+		Render(self, 'n_index.htm', {})
 
 	def post(self):
 		email = self.request.get('email')
@@ -107,7 +125,7 @@ class LoginHandler(BaseHandler):
 			u = q.get()
 			if u.password == hashlib.sha256(password + u.salt).hexdigest():
 				self.session['email'] = email
-				Render(self, 'index.htm', {})
+				Render(self, 'n_index.htm', {})
 			else:
 				Render(self, 'error.htm', {'errorcode':'401'})
 		else:
@@ -115,19 +133,50 @@ class LoginHandler(BaseHandler):
 
 class LogoutHandler(BaseHandler):
 	def get(self):
-		self.session.clear()
-		Render(self, "index.htm")
+		if self.session.get('email'):
+			self.session.pop('email')
+			self.session.clear()
+		Render(self, 'n_index.htm', {})
 
 	def post(self):
-		self.session.clear()
-		Render(self, "index.htm")
+		if self.session.get('email'):
+			self.session.pop('email')
+			self.session.clear()
+		Render(self, 'n_index.htm', {})
+
+class UserpwHandler(BaseHandler):
+	def get(self):
+		Render(self, 'n_index.htm', {})
+
+	def post(self):
+		email = self.request.get('email')
+		query = db.Query(User)
+		q = query.filter("email ==",email)
+		if q.count() == 1:
+			# 유저가 존재하는 경우
+			user = UserRecovery()
+			user.email = email
+			code = Salt()
+			user.code = code
+			t = int(time.time()) + 1800
+			user.expire = t
+			user.put()
+			message = mail.EmailMessage(sender="no-reply@chickenintheweb.appspotmail.com",
+                            subject="치킨인더웹 비밀번호 재설정 코드입니다.")
+			message.to = email
+			message.body = """
+			비밀번호 재설정 코드는 %s 입니다.
+			""" % code
+			message.send()
+		else:
+			Render(self, 'error.htm', {'errorcode':'400'})
 
 class MainHandler(BaseHandler):
     def get(self):
 		logging.info(self.request.path)
 		if Render(self,self.request.path):
 			return
-		Render(self, 'index.htm', {})
+		Render(self, 'n_index.htm', {})
 
 config = {}
 
@@ -135,7 +184,7 @@ config['webapp2_extras.sessions'] = {
     'secret_key': 'dc458da48fa171a071a547a07d8e13f25dd2ed714a03f4d6fbae331e6b711139',
 }
 
-app = webapp2.WSGIApplication([('/login', LoginHandler), ('/register', RegisterHandler), ('/logout', LogoutHandler), ('/.*', MainHandler)], debug=True, config=config)
+app = webapp2.WSGIApplication([('/login', LoginHandler), ('/register', RegisterHandler), ('/logout', LogoutHandler), ('/findpw', UserpwHandler), ('/.*', MainHandler)], debug=True, config=config)
 
 def main():
 	app.run()
