21e57b8837664b93f68087fc78e640671eb4f1e3
devholic : MBPr (Mac) <devholic@plusquare.com>
2014-12-20 00:13:32 +0900

    commit #15
    - 솔직히 지금 뭐하고있는지 모르겠음
    - db 모델 추가 및 수정
    - 치킨 디테일 페이지 만드는중
    - 브랜드 / 동네치킨 페이지 통합 (request로 구분)
    - 브랜드 / 동네치킨 가게 리스트 보여주기 구현
    - 프랜차이즈 가입 옵션 추가
    - 치킨 생성 옵션 리밋
    - 메뉴 개수 조절 (수정)

--- main.py
+++ main.py
@@ -15,44 +15,54 @@ from google.appengine.api import images
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
-class User(db.Model):
+class User(db.Model): # 유저
 	email = db.StringProperty() # 이메일
 	password = db.StringProperty() # 비밀번호
-	name = db.StringProperty() # 이름
-	storename = db.StringProperty() # 판매자 전용 속성 : 이름
+	name = db.StringProperty() # 이름 / 업체명
 	address = db.StringProperty() # 주소
 	number = db.StringProperty() # 전화번호
 	salt = db.StringProperty() # Salt
-	created_at = db.DateProperty(auto_now_add=True) # Created At
+	created_at = db.DateTimeProperty(auto_now_add=True) # Entity 생성시간
 	isSeller = db.BooleanProperty() # 판매자인지
-	isBrandSeller = db.BooleanProperty() 
+	isBrandSeller = db.BooleanProperty() # 프랜차이즈 기업 회원인지
 	isAdmin = db.BooleanProperty() # 관리자인지
 
-class Chicken(db.Model): # 판매 치킨
-	seller = db.ReferenceProperty() # 셀러정보
-	name = db.StringProperty() # 이름
+class Store(db.Model): # 스토어
+	seller = db.ReferenceProperty() # 판매자 (User)
+	intro = db.StringProperty() # 한줄소개 
+	thumb = db.BlobProperty() # 썸네일
+	isBrand = db.BooleanProperty() # 브랜드 치킨인지
+	created_at = db.DateTimeProperty(auto_now_add=True) # Entity 생성시간
+
+class Chicken(db.Model): # 치킨
+	seller = db.ReferenceProperty() # 판매자 (Store)
+	name = db.StringProperty() # 치킨 이름
 	quantity = db.IntegerProperty() # 수량
 	price = db.IntegerProperty() # 가격
-	intro = db.StringProperty() # 이름
-	thumb = db.BlobProperty()
+	intro = db.StringProperty() # 한줄소개
+	thumb = db.BlobProperty() # 썸네일
+	created_at = db.DateTimeProperty(auto_now_add=True) # Entity 생성시간
 
 class ChickenOption(db.Model): # 치킨 옵션
-	chicken = db.ReferenceProperty() # 셀러정보
-	name = db.StringProperty() # 이름
+	chicken = db.ReferenceProperty() # 치킨 
+	name = db.StringProperty() # 옵션 이름
 	quantity = db.IntegerProperty() # 수량
 	price = db.IntegerProperty() # 가격
 
-class ChickenImg(db.Model): # 치킨 옵션
-	chicken = db.ReferenceProperty() # 셀러정보
-	u = db.StringProperty() # 이름
-	f = db.BlobProperty()
+class ChickenImage(db.Model): # 치킨 이미지
+	chicken = db.ReferenceProperty() # 치킨
+	f = db.BlobProperty() # 치킨 파일 
 
-class UserRecovery(db.Model):
+class UserRecovery(db.Model): # 유저 비밀번호 찾기용 복구 코드 관리 Model
 	email = db.StringProperty() # 이메일
 	code = db.StringProperty() # 코드
-	expire = db.IntegerProperty()
+	expire = db.IntegerProperty() # 만료시간 (ms)
 
-def Render(handler, path = "n_index.htm", values ={}):
+class Wallet(db.Model): # 지갑 모델
+	user = db.ReferenceProperty() # 유저
+	money = db.IntegerProperty() # 월렛 잔고
+
+def Render(handler, path = "n_index.htm", values ={}): # 템플릿 렌더링 
 	fpath = os.path.join(os.path.dirname(__file__), 'templates/' + path)
 	if not os.path.isfile(fpath):
 		return False
@@ -71,102 +81,116 @@ def Render(handler, path = "n_index.htm", values ={}):
 	handler.response.out.write(unicode(outstr))
 	return True
 
-def Salt():
+def Salt(): # 비밀번호 암호화용 Salt 생성 메소드
 	alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	saltstring = ""
-	for i in range(16):
+	saltstring = "" # 결과를 담을 스트링 
+	for i in range(16): # Salt는 16자리
 		saltstring+=random.choice(alphabet)
-	return saltstring
+	return saltstring # 생성된 Salt를 return
 
-class BaseHandler(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler): # Webapp2에서 제공하는 Session을 사용하기 위한 
     def dispatch(self):
-        # Get a session store for this request.
+        # 요청에 대한 session_store 가져오기
         self.session_store = sessions.get_store(request=self.request)
         try:
-            # Dispatch the request.
+            # Request dispatch
             webapp2.RequestHandler.dispatch(self)
         finally:
-            # Save all sessions.
+            # 세션 저장
             self.session_store.save_sessions(self.response)
 
     @webapp2.cached_property
     def session(self):
-        # Returns a session using the default cookie key.
+        # 기본 Cookie 키를 사용하는 세션 리턴
         return self.session_store.get_session()
 
-class RegisterHandler(BaseHandler):
+class RegisterHandler(BaseHandler): # 회원가입 핸들러
+	# get method 
 	def get(self):
+		# 회원가입 페이지 보여주기
 		Render(self, 'n_register.htm', {})
-
+	# post method
 	def post(self):
-		captcha = self.request.get('g-recaptcha-response')
-		url = "https://www.google.com/recaptcha/api/siteverify?secret=6Ldi6f4SAAAAAJ5WXvkKk1cSzc7L9C1CALkCnITs&response="+captcha
-		chk = urllib2.urlopen(url)
-		data = json.load(chk)
+		captcha = self.request.get('g-recaptcha-response') # recaptcha response 받기 
+		url = "https://www.google.com/recaptcha/api/siteverify?secret=6Ldi6f4SAAAAAJ5WXvkKk1cSzc7L9C1CALkCnITs&response="+captcha # recaptcha response 검증 URL
+		chk = urllib2.urlopen(url) # 검증 url open
+		data = json.load(chk) # response를 json으로 파싱 
 		if data['success']:
-			# Captcha 성공
-			req_email = self.request.get('email')
-			query = db.Query(User)
-			q = query.filter("email ==", req_email)
+			# recaptcha 성공
+			req_email = self.request.get('email') # 사용자 입력 이메일 가져오기
+			query = db.Query(User) # 유저 쿼리
+			q = query.filter("email ==", req_email) # 쿼리 필터링
 			if q.count() == 1:
 				# 유저가 존재하는 경우
-				u = q.get()
-				req_pw = self.request.get('password')
+				u = q.get() # 유저 가져오기
+				req_pw = self.request.get('password') # 사용자 입력 비밀번호 가져오기 
 				# 로그인 시도
-				if u.password == hashlib.sha256(req_pw + u.salt).hexdigest():
-					self.session['email'] = req_email
-					self.redirect("/")
+				if u.password == hashlib.sha256(req_pw + u.salt).hexdigest(): # 입력된 비밀번호 hash값과 Datastore에 저장된 유저 비밀번호 해쉬값 비교
+					self.session['email'] = req_email # 세션에 이메일 추가
+					self.session['name'] = u.name # 세션에 이름 추가
+					if u.isSeller: # 판매자인경우
+						self.session['seller'] = 'true' # 판매자 속성 추가
+					self.redirect("/") # Main으로 redirect (로그인 처리 완료)
 				else:
-				# 만약 아니라면 이미 있는 계정이라고 form update
+				# 만약 아니라면 이미 있는 계정이라고 alert
 					Render(self, 'n_register.htm', {'err': '이미 있는 계정입니다.'})
 			else:
-				# 유저를 추가
-				saltstring = Salt()
-				user = User()
-				user.email = req_email
-				user.password = hashlib.sha256(self.request.get('password') + saltstring).hexdigest() # Hash 후 저장
-				user.name = self.request.get('name')
-				user.address = self.request.get('address')
-				user.number = self.request.get('number')
-				user.salt = saltstring # Salt
+				# 유저가 없는 경우 유저를 추가
+				saltstring = Salt() # salt 생성
+				user = User() # User 생성 
+				user.email = req_email # 이메일 가져오기
+				user.password = hashlib.sha256(self.request.get('password') + saltstring).hexdigest() # 비밀번호 Hash 후 저장
+				user.name = self.request.get('name') # 사용자 입력 이름 가져오기 
+				user.address = self.request.get('address') # 사용자 입력 주소 가져오기 
+				user.number = self.request.get('number') # 사용자 입력 번호 가져오기 
+				user.salt = saltstring # salt
 				user.isSeller = False # 판매자 False
-				user.isBrandSeller = False
+				user.isBrandSeller = False # 프랜차이즈 판매자 False
 				user.isAdmin = False # 관리자 False
-				user.put()
-				self.session['email'] = user.email
-				self.session['name'] = user.name
-				self.redirect("/")
+				k = user.put() # User 저장
+				w = Wallet() # 월렛 생성
+				w.user = k # Return 받은 User key 지정
+				w.money = 10000 # 기본 적립금 만원
+				w.put() # 월렛 저장
+				self.session['email'] = user.email # 세션에 이메일 추가
+				self.session['name'] = user.name # 세션에 이름 추가
+				self.redirect("/") # Main으로 redirect (로그인 처리 완료)
 		else:
-			if "response" in str(data['error-codes']):
-				Render(self, 'n_register.htm', {'err': 'reCAPTCHA가 정상적으로 확인되지 않았습니다. 다시 시도해주세요.'})
+			# recaptcha 실패
+			if "response" in str(data['error-codes']): # 에러코드 체크
+				# recaptcha 에러인 경우
+				Render(self, 'n_register.htm', {'err': 'reCAPTCHA가 정상적으로 확인되지 않았습니다. 다시 시도해주세요.'}) 
 			else:
+				# 그 외의 에러인 경우
 				Render(self, 'n_register.htm', {'err': '시스템 에러입니다. 잠시후 다시 시도해주세요.'})
 
-class SellerRegisterHandler(BaseHandler):
+class SellerRegisterHandler(BaseHandler): # 판매자 회원가입 핸들러
+	# get method 
 	def get(self):
+		# 판매자 회원가입 페이지 보여주기
 		Render(self, 'n_register_seller.htm', {})
-
+	# post method 
 	def post(self):
-		captcha = self.request.get('g-recaptcha-response')
-		url = "https://www.google.com/recaptcha/api/siteverify?secret=6Ldi6f4SAAAAAJ5WXvkKk1cSzc7L9C1CALkCnITs&response="+captcha
-		chk = urllib2.urlopen(url)
-		data = json.load(chk)
+		captcha = self.request.get('g-recaptcha-response') # recaptcha response 받기
+		url = "https://www.google.com/recaptcha/api/siteverify?secret=6Ldi6f4SAAAAAJ5WXvkKk1cSzc7L9C1CALkCnITs&response="+captcha # recaptcha response 검증 URL
+		chk = urllib2.urlopen(url) # 검증 url open
+		data = json.load(chk) # response를 json으로 파싱 
 		if data['success']:
 			# Captcha 성공
-			req_email = self.request.get('email')
-			query = db.Query(User)
-			q = query.filter("email ==", req_email)
+			req_email = self.request.get('email') # 사용자 입력 이메일 가져오기
+			query = db.Query(User) # 유저 쿼리
+			q = query.filter("email ==", req_email) # 쿼리 필터링
 			if q.count() == 1:
 				# 유저가 존재하는 경우
-				u = q.get()
-				req_pw = self.request.get('password')
+				u = q.get() # 유저 가져오기
+				req_pw = self.request.get('password') # 사용자 입력 비밀번호 가져오기 
 				# 로그인 시도
-				if u.password == hashlib.sha256(req_pw + u.salt).hexdigest():
-					self.session['email'] = req_email
-					self.session['name'] = user.name
-					if u.isSeller:
-						self.session['seller'] = 'true'
-					self.redirect("/")
+				if u.password == hashlib.sha256(req_pw + u.salt).hexdigest(): # 입력된 비밀번호 hash값과 Datastore에 저장된 유저 비밀번호 해쉬값 비교
+					self.session['email'] = req_email # 세션에 이메일 추가
+					self.session['name'] = u.name # 세션에 이름 추가
+					if u.isSeller: # 판매자인경우
+						self.session['seller'] = 'true' # 판매자 속성 추가
+					self.redirect("/") # Main으로 redirect (로그인 처리 완료)
 				else:
 				# 만약 아니라면 이미 있는 계정이라고 form update
 					Render(self, 'n_register_seller.htm', {'err': '이미 있는 계정입니다.'})
@@ -181,13 +205,25 @@ class SellerRegisterHandler(BaseHandler):
 				user.number = self.request.get('number')
 				user.salt = saltstring # Salt
 				user.isSeller = True # 판매자 False
-				user.isBrandSeller = False
 				user.isAdmin = False # 관리자 False
-				user.put()
-				self.session['email'] = user.email
-				self.session['name'] = user.name
-				self.session['seller'] = 'true'
-				self.redirect("/")
+				if self.request.get('brand'):
+					user.isBrandSeller = True # 판매자 False
+				else:
+					user.isBrandSeller = False # 판매자 False
+				uk = user.put()
+				s = Store()
+				s.seller = uk
+				s.isBrand = user.isBrandSeller
+				s.intro = unicode("치킨인더웹 페이지",'utf-8')
+				s.put()
+				w = Wallet()
+				w.user = uk # Return 받은 User key 지정
+				w.money = 0 # 기본 판매액 0원 
+				w.put() # 월렛 저장 
+				self.session['email'] = user.email # 세션에 이메일 추가
+				self.session['name'] = user.name # 세션에 이름 추가
+				self.session['seller'] = 'true' # 세션에 판매자 속성 추가
+				self.redirect("/") # Main으로 redirect (로그인 처리 완료)
 		else:
 			if "response" in str(data['error-codes']):
 				Render(self, 'n_register_seller.htm', {'err': 'reCAPTCHA가 정상적으로 확인되지 않았습니다. 다시 시도해주세요.'})
@@ -288,6 +324,7 @@ class MainHandler(BaseHandler):
 			query = db.Query(Chicken)
 			result = query.fetch(limit=100)
 			for c in result:
+				# https://www.python.org/dev/peps/pep-0378/
 				c.pricestr = '₩'+format(c.price,',d')
 			Render(self, 'n_index.htm', {'chicken_list':result})
 		else:
@@ -295,15 +332,88 @@ class MainHandler(BaseHandler):
 
 class BrandHandler(BaseHandler):
 	def get(self):
-		if self.session.get('email'):
-			Render(self, 'n_brand.htm', {})
+		if self.session.get('email') and not self.session.get('seller'):
+			if self.request.get("cid"):
+				q = Chicken.get_by_id(int(self.request.get("cid")))
+				if q:
+					q.pricestr = '₩'+format(q.price,',d')
+					oq = db.Query(ChickenOption)
+					oq.filter("chicken =", q.key())
+					o = oq.fetch(limit=30)
+					for op in o:
+						# https://www.python.org/dev/peps/pep-0378/
+						op.pricestr = '₩'+format(op.price,',d')
+					if len(o) != 0:
+						Render(self, 'n_chicken.htm',{'chicken':q,'option_list':o})
+					else:
+						Render(self, 'n_chicken.htm',{'chicken':q})
+				else:
+					query = db.Query(Store)
+					query.filter("isBrand =", True)
+					result = query.fetch(limit=100)
+					Render(self, 'n_store.htm', {'request':'storelist','store_list':result, 'type':'brand'})
+			elif self.request.get("id"):
+				q = Store.get_by_id(int(self.request.get("id")))
+				if q:
+					cquery = db.Query(Chicken)
+					cquery.filter("seller =", q.seller)
+					result = cquery.fetch(limit=100)
+					for c in result:
+						# https://www.python.org/dev/peps/pep-0378/
+						c.pricestr = '₩'+format(c.price,',d')
+					Render(self, 'n_store.htm', {'request':'store','store':q, 'chicken_list':result})
+				else:
+					query = db.Query(Store)
+					query.filter("isBrand =", True)
+					result = query.fetch(limit=100)
+					Render(self, 'n_store.htm', {'request':'storelist','store_list':result, 'type':'brand'})
+			else:
+				query = db.Query(Store)
+				query.filter("isBrand =", True)
+				result = query.fetch(limit=100)
+				Render(self, 'n_store.htm', {'request':'storelist','store_list':result, 'type':'brand'})
 		else:
 			Render(self, 'n_index.htm', {})
 
 class LocalHandler(BaseHandler):
 	def get(self):
-		if self.session.get('email'):
-			Render(self, 'n_local.htm', {})
+		if self.session.get('email') and not self.session.get('seller'):
+			if self.request.get("cid"):
+				q = Chicken.get_by_id(int(self.request.get("cid")))
+				if q:
+					q.pricestr = '₩'+format(q.price,',d')
+					oq = db.Query(ChickenOption)
+					oq.filter("chicken =", q.key())
+					o = oq.fetch(limit=30)
+					for op in o:
+						# https://www.python.org/dev/peps/pep-0378/
+						op.pricestr = '₩'+format(op.price,',d')
+					Render(self, 'n_chicken.htm',{'chicken':q,'option_list':o})
+				else:
+					query = db.Query(Store)
+					query.filter("isBrand =", False)
+					result = query.fetch(limit=100)
+					Render(self, 'n_store.htm', {'request':'storelist','store_list':result, 'type':'local'})
+			elif self.request.get("id"):
+				q = Store.get_by_id(int(self.request.get("id")))
+				if q:
+					cquery = db.Query(Chicken)
+					cquery.filter("seller =", q.seller)
+					result = cquery.fetch(limit=100)
+					for c in result:
+						# https://www.python.org/dev/peps/pep-0378/
+						c.pricestr = '₩'+format(c.price,',d')
+					Render(self, 'n_store.htm', {'request':'store','store':q, 'chicken_list':result})
+				else:
+					query = db.Query(Store)
+					query.filter("isBrand =", False)
+					result = query.fetch(limit=100)
+					Render(self, 'n_store.htm', {'request':'storelist','store_list':result, 'type':'local'})
+			else:
+				query = db.Query(Store)
+				query.filter("isBrand =", False)
+				result = query.fetch(limit=100)
+				Render(self, 'n_store.htm', {'request':'storelist','store_list':result, 'type':'local'})
 		else:
 			Render(self, 'n_index.htm', {})
 
@@ -323,7 +433,6 @@ class SellerNewChickenHandler(BaseHandler):
 
 	def post(self):
 		if self.session.get('email') and self.session.get('seller'):
-			#self.request.get_all()
 			query = db.Query(User)
 			q = query.filter("email ==",self.session.get('email'))
 			u = q.get()
@@ -335,8 +444,26 @@ class SellerNewChickenHandler(BaseHandler):
 			c.intro = self.request.get("intro")
 			ti = self.request.get("thumbimage")
 			c.thumb = db.Blob(ti)
-			c.isBrand = u.isBrandSeller
-			c.put()
+			ck = c.put()
+			i = self.request.get_all("fimage")
+			if i:
+				for image in i:
+					ci = ChickenImage()
+					ci.chicken = ck
+					ci.f = db.Blob(image)
+					ci.put()
+			on = self.request.get_all("oname")
+			if on:
+				op = self.request.get_all("oprice")
+				oq = self.request.get_all("oquantity")
+				for oc in range(len(on)):
+					o = ChickenOption()
+					o.chicken = ck
+					o.name = on[oc]
+					o.quantity = int(oq[oc])
+					o.price = int(op[oc])
+					o.put()
+			self.redirect('/seller/chicken')
 
 class TermsHandler(BaseHandler):
 	def get(self):
@@ -352,12 +479,12 @@ class ImageHandler(webapp2.RequestHandler):
 			k = self.request.get("id")
 			r = db.get(k)
 			if r.thumb:
-				self.response.headers['Content-Type'] = 'image/png'
+				self.response.headers['Content-Type'] = 'image/jpg'
 				self.response.out.write(r.thumb)
 			else:
-				self.response.out.write("noimg")
+				self.redirect('/resources/holder.jpg')
 		else:
-			self.redirect('/')
+			self.redirect('/resources/holder.jpg')
 
 config = {}
 
@@ -365,7 +492,11 @@ config['webapp2_extras.sessions'] = {
     'secret_key': 'dc458da48fa171a071a547a07d8e13f25dd2ed714a03f4d6fbae331e6b711139',
 }
 
-app = webapp2.WSGIApplication([('/login', LoginHandler), ('/register', RegisterHandler), ('/register/seller', SellerRegisterHandler), ('/logout', LogoutHandler), ('/findpw', UserpwHandler), ('/chicken/brand', BrandHandler), ('/chicken/local', LocalHandler), ('/order/bucket', OrderBucketHandler), ('/mypage', MypageHandler), ('/seller/add', SellerNewChickenHandler), ('/terms', TermsHandler), ('/security', SecurityHandler), ('/blob/image', ImageHandler), ('/.*', MainHandler)], debug=True, config=config)
+app = webapp2.WSGIApplication([('/login', LoginHandler), ('/register', RegisterHandler), 
+	('/register/seller', SellerRegisterHandler), ('/logout', LogoutHandler), ('/findpw', UserpwHandler), 
+	('/chicken/brand', BrandHandler), ('/chicken/local', LocalHandler), ('/order/bucket', OrderBucketHandler), 
+	('/mypage', MypageHandler), ('/seller/add', SellerNewChickenHandler), ('/terms', TermsHandler), 
+	('/security', SecurityHandler), ('/blob/image', ImageHandler), ('/.*', MainHandler)], debug=True, config=config)
 
 def main():
 	app.run()
